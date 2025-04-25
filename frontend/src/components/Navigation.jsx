1f881188857f82feba6f8f3234aaf7fba6701da9
DTS-Challenge <mrbuzzles@outlook.com>
2025-04-25 15:04:31 +0100

    feat: added navigation component

---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,25 @@
+export default function Navigation() {
+  return (
+    <div className=" govuk-width-container">
+      <nav class="govuk-breadcrumbs" aria-label="Breadcrumb">
+        <ol class="govuk-breadcrumbs__list">
+          <li class="govuk-breadcrumbs__list-item">
+            <a class="govuk-breadcrumbs__link" href="#">
+              HMCTS Portal
+            </a>
+          </li>
+          <li class="govuk-breadcrumbs__list-item">
+            <a class="govuk-breadcrumbs__link" href="#">
+              Your work and cases
+            </a>
+          </li>
+          <li class="govuk-breadcrumbs__list-item">
+            <a class="govuk-breadcrumbs__link" href="#">
+              Task manager
+            </a>
+          </li>
+        </ol>
+      </nav>
+    </div>
+  );
+}
