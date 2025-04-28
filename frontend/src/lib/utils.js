62d7ce253c24aa260063ed65810cd758efb2dada
DTS-Challenge <mrbuzzles@outlook.com>
2025-04-28 01:30:08 +0100

    feat: added utility date formatting functions

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,13 +1,37 @@
-export function formatDate(dateString) {
-  const date = new Date(dateString);
+export function formatDate(day, month, year) {
+  const formattedMonth = month.padStart(2, "0");
+  const formattedDay = day.padStart(2, "0");
+  return `${year}-${formattedMonth}-${formattedDay}`;
+}
 
-  const day = date.getDate();
-  const month = date.toLocaleString("en-GB", { month: "short" });
-  const year = date.getFullYear();
+export function validateDate(day, month, year) {
+  const date = new Date(year, month - 1, day);
+  return (
+    date.getDate() === parseInt(day) &&
+    date.getMonth() === month - 1 &&
+    date.getFullYear() === parseInt(year)
+  );
+}
 
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-  const ampm = date.getHours() >= 12 ? "PM" : "AM";
+export function formatDateTimeZone(dateString, timeZone) {
+  
+  if (typeof dateString !== 'string') {
+    throw new Error('Invalid dateString: expected a string');
+  }
 
-  return `${day} ${month} ${year}, ${hours}:${minutes} ${ampm}`;
+  const dateWithUTC = dateString.endsWith("Z") ? dateString : `${dateString}Z`;
+  const date = new Date(dateWithUTC);
+
+  const options = {
+    weekday: "short",
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true, 
+    timeZone: timeZone, 
+  };
+  const formattedDate = new Intl.DateTimeFormat("en-GB", options).format(date);
+  return formattedDate;
 }
