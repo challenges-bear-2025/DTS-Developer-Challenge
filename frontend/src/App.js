c560eec1a70d7e7d7769a6be303224d8acf79de1
DTS-Challenge <mrbuzzles@outlook.com>
2025-04-28 01:31:56 +0100

    feat: added updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import { initGovUK } from "./utils/govuk-init";
-import './styles/main.scss'
+import "./styles/main.scss";
 import { useEffect } from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TaskManager from "./views/TaskManager";
+import Navigation from "./components/Navigation";
 
 function App() {
   useEffect(() => {
     initGovUK();
-  }, [])
+  }, []);
 
   return (
-    <div className="App">
-      <Header serviceName="Task Management System"/>
+    <div>
+      <Header />
+      <Navigation />
+      <TaskManager />
+      <Footer />
     </div>
   );
 }
